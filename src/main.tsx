51c4d01d7594d0f8c20a0b9de340d2b7f8336d3b
avas27027 <alvas27027@gmail.com>
2024-08-15 14:44:30 -0500

    install Nextui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import LandingPage from './app/core/components/landing-page/LandingPage'
+import { NextUIProvider } from '@nextui-org/react'
 import './styles.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LandingPage />
+    <NextUIProvider>
+      <LandingPage />
+    </NextUIProvider>
   </StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -24,6 +26,7 @@ export default {
       }
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 
